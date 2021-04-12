1def0b3ac938fd360741399ab5b1c793d15dc5a3
Pablo Estevez <pestevez@fb.com>
2021-04-12 14:23:55 -0400

    Bump version to 1.10.2

--- src/Facebook/InstantArticles/Elements/InstantArticle.php
+++ src/Facebook/InstantArticles/Elements/InstantArticle.php
@@ -34,7 +34,7 @@ use Facebook\InstantArticles\Validators\Type;
 
 class InstantArticle extends Element implements ChildrenContainer, InstantArticleInterface
 {
-    const CURRENT_VERSION = '1.10.0';
+    const CURRENT_VERSION = '1.10.2';
 
     /**
      * The meta properties that are used on <head>
